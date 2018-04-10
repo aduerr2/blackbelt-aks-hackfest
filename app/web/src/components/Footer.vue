a46476c0ac1f5b9c116b9877fe142498c47299e7
aduerr2 <38252674+aduerr2@users.noreply.github.com>
2018-04-10 14:12:45 -0400

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-       Anna Duerr Azure Global Blackbelt Team 
+       Anna Duerr 10th times Azure Global Blackbelt Team 
       </div>
       <div class="col-lg-6">
       </div>
